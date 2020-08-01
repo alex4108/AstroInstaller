f6b9c1bc7e842d441e4e571a309e8cd435bfb403
Alex Schittko <alex4108@live.com>
2020-08-01 07:18:29 -0500

    Revert "Revert "1.1.7 Releases""
    This reverts commit 73f92152b36891a08deac1192275acd04171026a.

--- build.ps1
+++ build.ps1
@@ -9,4 +9,4 @@ $file = "install-astroneer-server.ps1"
 $regex = '\$version\s+\=\s+"(?:(?!\.)(?:.|\n))*\.(?:(?!\.)(?:.|\n))*\.(?:(?!")(?:.|\n))*"'
 (Get-Content $PSScriptRoot\$file) -replace $regex, "`$version = `"$version`"" | Set-Content $PSScriptRoot\$file
 ps2exe -inputFile "$PSScriptRoot\install-astroneer-server.ps1" -outputFile "$PSScriptRoot\install-astroneer-server.exe" -verbose -requireAdmin -title "Astroneer Server Installer"
-Compress-Archive "$PSScriptRoot\install-astroneer-server.exe" "$PSScriptRoot\install-astroneer-server-$version.zip"
+Compress-Archive "$PSScriptRoot\install-astroneer-server.exe" "$PSScriptRoot\install-astroneer-server.zip"
