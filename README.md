8886f02ed4d4dedb027d1a3cb2fefe305a7f07ab
Alex Schittko <alex4108@live.com>
2020-08-02 01:58:45 -0500

    v2 release
    Added GUI option, Added Transcript Logging, Fixed handling of blank inputs, fixed handling astroneer config file generation

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ![Astroneer Logo](https://astroneer.space/presskit/astroneer/images/header.png)
 
 [![Build Status](https://travis-ci.com/alex4108/astroneer-server-deployment.svg?branch=master)](https://travis-ci.com/alex4108/astroneer-server-deployment)
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Downloads][downloads-shield]][downloads-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
 
 This module lets you quickly deploy an [Astroneer Dedicated Server](https://blog.astroneer.space/p/astroneer-dedicated-server-details/)
 
@@ -21,6 +27,7 @@ Did I save you some time?  [Buy me a :coffee::smile:](https://venmo.com/alex-sch
 * `Server Name`: This will be the name of your server
 * `Max FPS`: Set to 30 if you're unsure.  
 * `Server Password`: A server password if you wish, or just hit enter for no password.
+* `Use AstroLauncher as GUI [y/n]`: _Recommended_ If you'd like to install [AstroLauncher](https://www.github.com/ricky-davis/AstroLauncher) to manage the server.
 
 ## Advanced Method
 
@@ -33,12 +40,27 @@ Did I save you some time?  [Buy me a :coffee::smile:](https://venmo.com/alex-sch
 * `-serverName "The server name"`
 * `-serverPassword "A Super Secure Password"`
 * `-maxFPS 60`
+* `-useGUI y` or `-useGUI n` to install [AstroLauncher](https://www.github.com/ricky-davis/AstroLauncher)
+* `-autoReboot y` _Optional_ If set to y, the server will be rebooted automatically after installation if needed.  Otherwise, if a reboot is needed, the script will exit and the server will not be able to start until the reboot is complete.
 
-#### Full example to bypass all prompts
+#### Unattended installation
 
-* `powershell.exe -executionpolicy bypass -file "install-astroneer-server.ps1" -ownerName "Guy" -serverName "Guys Lair" -serverPassword "Guy123" -maxFPS 60`
+* `powershell.exe -executionpolicy bypass -file "install-astroneer-server.ps1" -ownerName "Guy" -serverName "Guys Lair" -serverPassword "Guy123" -maxFPS 60 -useGUI y`
+
+# Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
 
 # Credits
 
-[MScholtes](https://github.com/MScholtes/PS2EXE)
-[hjorslev](https://github.com/hjorslev/SteamPS)
+[MScholtes](https://github.com/MScholtes/PS2EXE) - PS2EXE allows me to build this in a way that's easy to use by masses
+
+[hjorslev](https://github.com/hjorslev/SteamPS) - Wrapper that helped in speeding up usage of SteamCMD
+
+[ricky-davis](https://www.github.com/ricky-davis/AstroLauncher) - Web based server management utility for Astroneer
--- install-astroneer-server.ps1
+++ install-astroneer-server.ps1
@@ -1,61 +1,61 @@
-Param($ownerName, $serverName, $maxFPS, $serverPassword)
-Start-Transcript -Path $PSScriptRoot
+Param($ownerName, $serverName, $maxFPS, $serverPassword, $useGUI, $autoReboot)
+Start-Transcript -Path "$PSScriptRoot\install-astroneer-server.log" -Append
 #Param($ownerName, $serverName, $maxFPS, $serverPassword, [switch]$installAsService)
 
-#https://stackoverflow.com/questions/150161/waiting-for-user-input-with-a-timeout
-Function TimedPrompt($prompt,$secondsToWait){   
-    Write-Host -NoNewline $prompt
-    $secondsCounter = 0
-    $subCounter = 0
-    While ( (!$host.ui.rawui.KeyAvailable) -and ($count -lt $secondsToWait) ){
-        start-sleep -m 10
-        $subCounter = $subCounter + 10
-        if($subCounter -eq 1000)
-        {
-            $secondsCounter++
-            $subCounter = 0
-            Write-Host -NoNewline "."
-        }       
-        If ($secondsCounter -eq $secondsToWait) { 
-            Write-Host "`r`n"
-            return $false;
-        }
-    }
-    Write-Host "`r`n"
-    return $true;
-}
 
-
-$version = "1.1.9"
+$version = "2.0"
 Write-Host "install-astroneer-server $version"
 
-while ( $ownerName -eq $null ) { 
-    $ownerName = Read-Host -Prompt "Enter Owner's Steam Name "
-    if (!$ownerName) { 
+while ( $null -eq $ownerName -or $ownerName -eq "" ) { 
+    $ownerName = Read-Host -Prompt "Enter Owner's Steam Name"
+    if ($null -eq $ownerName -or $ownerName -eq "") { 
         Write-Warning -Message "No Steam Name Given.  Try again."
     }
 }
 
-while ( $serverName -eq $null ) { 
-    $serverName = Read-Host -Prompt "Enter Server Name "
+while ( $null -eq $serverName ) { 
+    $serverName = Read-Host -Prompt "Enter Server Name"
     if (!$serverName) { 
         Write-Warning -Message "No Server Name Given.  Try again."
+        $serverName = $null
     }
 }
 
-while ( $maxFPS -eq $null ) { 
-    $maxFPS = Read-Host -Prompt "Enter Server Max FPS "
+while ( $null -eq $maxFPS ) { 
+    $maxFPS = Read-Host -Prompt "Enter Server Max FPS"
     if (!$maxFPS) { 
         Write-Warning -Message "No Max FPS Given Given.  Use 30 if you're unsure."
+        $maxFPS = $null
     }
 }
 
-
-if ( $serverPassword -eq $null ) { 
+if ( $null -eq $serverPassword ) { 
     $serverPassword = Read-Host -Prompt "Enter Server Password or leave blank"
     if (!$serverPassword) { 
         Write-Warning -Message "Your server is starting without a password.  BEWARE."
     }
+}
+
+while ( $useGUI -ne 'y' -and $useGUI -ne 'n' ) { 
+    $useGUI = Read-Host -Prompt "Use AstroLauncher GUI for Management (RECOMMENDED) [y/n]"
+    if ($useGUI -eq 'y') { 
+        $useGUI = $true
+        break
+    }
+    elseif ($useGUI -eq 'n') { 
+        $useGUI = $false
+        break
+    }
+    else {
+        Write-Warning -Message "Enter y or n"
+    }
+}
+
+if ($autoReboot -eq 'y') { 
+    $autoReboot = $true
+}
+else {
+    $autoReboot = $false
 }
 
 #while ( $installAsService -eq $null ) {
@@ -71,12 +71,13 @@ Write-Host "ASTRO CONFIG"
 Write-Host "Owner Name: [$ownerName]"
 Write-Host "Server Name: [$serverName]"
 Write-Host "Max FPS: [$maxFPS]"
-Write-Host "Server Password: [$serverPassword]"
+Write-Host "AstroLauncher GUI: [$useGUI]"
 
 Write-Host "Installing DotNet Framework"
 $dotNetResult = Install-WindowsFeature Net-Framework-Core
-if ($dotNetResult.RestartNeeded -eq "Yes") {
-  Write-Warning -Message "A reboot will be required after this installation is complete before Astroneer Dedicated Server will be functional."
+Write-Host $dotNetResult.RestartNeeded
+if ($dotNetResult.RestartNeeded -ne "No") {
+  Write-Warning -Message "A reboot will be required before Astroneer Dedicated Server will be functional."
   $reboot = $true
 }
 else {
@@ -91,16 +92,41 @@ $steamCMDResult = Install-SteamCMD -Force
 
 Write-Host "Installing ASTRONEER Dedicated Server"
 mkdir C:\SteamServers\
-Update-SteamApp -AppID 728470 -Path "C:\SteamServers\Astrnoeer" -Force
+Update-SteamApp -AppID 728470 -Path "C:\SteamServers\Astroneer" -Force
 
-$configFile = "C:\SteamServers\astrnoeer\Astro\Saved\Config\WindowsServer\AstroServerSettings.ini"
-$engineFile = "C:\SteamServers\astrnoeer\Astro\Saved\Config\WindowsServer\Engine.ini"
+$configFile = "C:\SteamServers\Astroneer\Astro\Saved\Config\WindowsServer\AstroServerSettings.ini"
+$engineFile = "C:\SteamServers\Astroneer\Astro\Saved\Config\WindowsServer\Engine.ini"
 
-Write-Host "Running UE4 Prerequisite Setup"
-C:\SteamServers\astrnoeer\Engine\Extras\Redist\en-us\UE4PrereqSetup_x64.exe /q
+Write-Host "Running Unreal Engine 4 Prerequisite Setup"
+C:\SteamServers\Astroneer\Engine\Extras\Redist\en-us\UE4PrereqSetup_x64.exe /quiet
+Wait-Process -Name "msiexec" -ErrorAction SilentlyContinue # Wait for the install to finish
+
 
 Write-Host "Generating Astroneer Config Files"
-C:\SteamServers\astrnoeer\AstroServer.exe /q
+Write-Warning "A warning will flash - it is safe to ignore."
+C:\SteamServers\Astroneer\AstroServer.exe /q
+Start-Sleep 5
+# Kill the Astro Setup as UE is already ready
+$astroSetup = Get-Process "AstroServer" -ErrorAction SilentlyContinue
+if ($astroSetup) {
+  $astroSetup.CloseMainWindow()
+  Start-Sleep 5
+  if (!$astroSetup.HasExited) {
+    $astroSetup | Stop-Process -Force
+  }
+}
+
+# Kill the UE4 Setup as we already did it
+$ue4Setup = Get-Process "UE4PrereqSetup_x64" -ErrorAction SilentlyContinue
+if ($ue4Setup) {
+  $ue4Setup.CloseMainWindow()
+  Start-Sleep 5
+  if (!$ue4Setup.HasExited) {
+    $ue4Setup | Stop-Process -Force
+  }
+}
+
+C:\SteamServers\Astroneer\AstroServer.exe /q
 
 Write-Host "Sleeping until the config file shows up"
 while (!(Test-Path $configFile)) { 
@@ -113,7 +139,7 @@ if ( (get-childitem $configFile).length -eq 0 ) {
 }
 
 Write-Host "Waiting for Astro to die"
-Wait-Process -Name "AstroServer.exe" -ErrorAction SilentlyContinue
+Wait-Process -Name "AstroServer" -ErrorAction SilentlyContinue
 Write-Host "Astro is dead!  Continuing..."
 # Astro is dead by now
 
@@ -142,8 +168,8 @@ Set-Content -Path $configFile -Value (get-content -Path $configFile | Select-Str
 Add-Content $configFile "OwnerName=$ownerName"
 
 Write-Host "Setting Port: 8777"
-Set-Content -Path $configFile -Value (get-content -Path $configFile | Select-String -Pattern "[URL]" -NotMatch)
-Set-Content -Path $configFile -Value (get-content -Path $configFile | Select-String -Pattern "Port=*" -NotMatch)
+Set-Content -Path $engineFile -Value (get-content -Path $engineFile | Select-String -Pattern "[URL]" -NotMatch)
+Set-Content -Path $engineFile -Value (get-content -Path $engineFile | Select-String -Pattern "Port=*" -NotMatch)
 Add-Content $engineFile "`r`n`r`n[URL]`r`nPort=8777"
 
 if ( $serverPassword -eq $null) {     
@@ -158,13 +184,52 @@ else {
 Add-Content $configFile "`r`n`r`n"
 Add-Content $engineFile "`r`n`r`n"
 
-if ($reboot -eq $true) { 
-    TimedPrompt "Press any key to abort reboot. Reboot will occur in 60 seconds." 60
-    shutdown /r /t 01
+# Install AstroLauncher
+if ($useGUI -eq $true) {
+    Write-Host "Downloading AstroLauncher"
+    $repo = "ricky-davis/AstroLauncher"
+    $filename = "AstroLauncher.exe"
+    $releasesUri = "https://api.github.com/repos/$repo/releases/latest"
+    $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filename ).browser_download_url
+    $iwrResponse = Invoke-WebRequest -Uri $downloadUri -OutFile "C:\SteamServers\Astroneer\AstroLauncher.exe" -PassThru
+}
+
+Write-Host "Adding Firewall Exceptions for port 8777"
+netsh advfirewall firewall add rule name="AstroServer" dir=in action=allow protocol=TCP localport=8777
+netsh advfirewall firewall add rule name="AstroServer" dir=in action=allow protocol=UDP localport=8777
+
+if ($reboot -eq $true) {
+    Write-Warning "Reboot scheduled for 60 seconds!"
+    if ($useGUI -eq $true) {
+        Write-Warning "When you come back, go to C:\SteamServers\Astroneer\ and run AstroLauncher.exe"
+    }
+    else {
+        Write-Warning "When you come back, go to C:\SteamServers\Astroneer\ and run AstroServer.exe"
+    }
+    if ($autoReboot -eq $true) { 
+        Write-Warning "System reboot scheduled for 60 seconds from now."
+        shutdown /r /t 60
+    }
+    else {
+        $data = Read-Host -Prompt "Press ENTER to reboot."
+        shutdown /r /t 60
+    }
+    
 }
 else {
     Write-Host "Starting Astro Server.  Have fun!"
-    Write-Host "If you need to make changes to the config file, ensure you kill the server first!"
-    $WmiProcess = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\SteamServers\astrnoeer\AstroServer.exe"
+    if ($useGUI -eq $true) {
+        Write-Host "Adding Firewall Exception for AstroLauncher TCP 5000"
+        netsh advfirewall firewall add rule name="AstroLauncher" dir=in action=allow protocol=TCP localport=5000
+        $WmiProcess = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\SteamServers\Astroneer\AstroLauncher.exe -p C:\SteamServers\Astroneer\"
+        $localIP = Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address
+        Write-Host "AstroLauncher should be available at http://$localIP`:5000"
+    }
+    else {
+        Write-Host "If you need to make changes to the config file, ensure you kill the server first!"
+        $WmiProcess = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\SteamServers\Astroneer\AstroServer.exe"
+    }
 }
+Write-Host "Installation is completed!  This script will exit in 2 minutes."
+Start-Sleep 120
 Stop-Transcript
